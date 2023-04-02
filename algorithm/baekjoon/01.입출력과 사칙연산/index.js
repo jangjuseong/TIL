7e36af0ca2c3f60d4e6f6663a4666c0ad5c08a77
juseong <115794886+jangjuseong@users.noreply.github.com>
2023-04-02 15:20:26 +0900

    Delete index.js

--- algorithm/baekjoon/01.입출력과 사칙연산/index.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const filePath =
-  process.platform === 'linux' ? '/dev/stdin' : `${__dirname}/../example.txt`;
-const input = fs.readFileSync(filePath).toString().split(' ');
