da4af928a521dce997601bae702f32f09101f200
juseong <jjs7280@gmail.com>
2023-04-07 13:19:28 +0900

    4월7일 알고리즘 문제 풀이

--- algorithm/baekjoon/03.반복문/15552.빠른 A+B/main.py
+++ algorithm/baekjoon/03.반복문/15552.빠른 A+B/main.py
@@ -1,2 +1,12 @@
 import sys
+
 sys.stdin = open('../../../example.txt', 'r', encoding='utf-8')
+
+a = int(sys.stdin.readline())
+
+for i in range(a):
+    b, c = map(int, sys.stdin.readline().split())
+    print(b+c)
+
+# 런타임 에러 남
+# 런타임 에러가 났던 이유: 백준 사이트에서 import sys를 같이 첨부를 안해줬기 때문.
