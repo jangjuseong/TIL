494147f3a0a0c947b815902fa14df732a9789c5f
juseong <jjs7280@gmail.com>
2023-05-09 20:48:41 +0900

    23/5/9 알고리즘 문제풀이

---
+++ algorithm/baekjoon/단계별로 풀어보기/14.집합과 맵/11478.서로 다른 부분 문자열의 개수/main.py
@@ -0,0 +1,20 @@
+import sys
+
+sys.stdin = open('../../../example.txt', 'r', encoding='utf-8')
+
+S = input()
+arr = []
+
+num = 0
+part = 1
+
+while True:
+    if part == len(S):
+        break
+    for i in range(0, len(S)-part+1):
+        arr.append(S[i:part+i])
+    part += 1
+
+arr.append(S)
+
+print(len(set(arr)))
