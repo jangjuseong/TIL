dfbe9cf41c63968f5e7309813d523a28eb2a51cf
juseong <jjs7280@gmail.com>
2023-05-12 23:59:48 +0900

    23/5/11 알고리즘 문제풀이

---
+++ algorithm/programmers/lv2/의상/main.py
@@ -0,0 +1,22 @@
+def solution(clothes):
+    answer = 0
+    dictionary = {}
+    
+    for i in range(len(clothes)):
+        if clothes[i][1] in dictionary:
+            dictionary[clothes[i][1]].append(clothes[i][0])
+        else:
+            dictionary[clothes[i][1]] = [clothes[i][0]]
+    
+    clothes_num = len(dictionary)
+    each_num = []
+    
+    
+    for values in dictionary.values():
+        each_num.append(len(values))
+        
+    while True:
+        for i in each_num:
+            answer += i
+    
+    return answer
