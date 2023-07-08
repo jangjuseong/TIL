dd8f06b8f0dfff7a390f2dee96c4976605134177
juseong <jjs7280@gmail.com>
2023-07-08 17:27:49 +0900

    23/7/8 알고리즘 문제풀이

---
+++ algorithm/programmers/lv2/월간 코드 챌린지 시즌3/n^2 배열 자르기/main.py
@@ -0,0 +1,8 @@
+def solution(n, left, right):
+    result = []
+    for i in range(left, right + 1):
+        row = i // n
+        col = i % n
+        num = max(row, col) + 1
+        result.append(num)
+    return result
