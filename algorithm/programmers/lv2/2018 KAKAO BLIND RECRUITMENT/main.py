d1574ea08507ed7c8d0e3e5393171587f0c84e3a
juseong <jjs7280@gmail.com>
2023-07-11 21:17:27 +0900

    23/7/11 알고리즘 문제풀이

---
+++ algorithm/programmers/lv2/2018 KAKAO BLIND RECRUITMENT/main.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+
+def solution(cacheSize, cities):
+    cache = deque(maxlen=cacheSize)
+    time = 0
+
+    for city in cities:
+        print(cache)
+        city = city.lower()
+
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            time += 1
+
+        else:
+            cache.append(city)
+            time += 5
+
+    return time
