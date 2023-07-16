c6298f021c1d06faec12087ebcb9cd052f1a67ba
juseong <jjs7280@gmail.com>
2023-07-17 00:41:39 +0900

    23/7/16 알고리즘 문제풀이

---
+++ algorithm/programmers/lv3/동적계획법(Dynamic Programming)/정수 삼각형/main.py
@@ -0,0 +1,18 @@
+def solution(triangle):
+    answer = 0
+
+    height = len(triangle)
+
+    dp = [[0] * (i + 1) for i in range(height)]
+    dp[0][0] = triangle[0][0]
+
+    for i in range(1, height):
+        for j in range(i + 1):
+            if j == 0:
+                dp[i][j] = dp[i - 1][j] + triangle[i][j]
+            elif j == i:
+                dp[i][j] = dp[i - 1][j-1] + triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+
+    return max(dp[height-1])
