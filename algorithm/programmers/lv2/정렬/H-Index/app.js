3a980475d6fda31fd23d433c6c058dd76113b36c
juseong <jjs7280@gmail.com>
2023-08-18 12:41:10 +0900

    23/8/18 알고리즘 문제풀이

---
+++ algorithm/programmers/lv2/정렬/H-Index/app.js
@@ -0,0 +1,12 @@
+function solution(citations) {
+  citations.sort((a, b) => b - a);
+
+  for (let i = 0; i < citations.length; i++) {
+    if (i + 1 <= citations[i]) {
+      continue;
+    }
+    return i;
+  }
+
+  return citations.length;
+}
